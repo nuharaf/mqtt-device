46c07f1c260446cc008395f4b7d52827b9fdb9b7
Arina Nuha <nuhamind2@gmail.com>
2019-02-09 11:46:26 +0700

    change header logging

--- umqtt.js
+++ umqtt.js
@@ -226,7 +226,7 @@ class umqtt {
             self.websocketServer.on("connection", function (ws, req) {
                 var stream = websocketStream(ws)
                 stream[WS_REQUEST] = req
-                self.logger.silly(`Request header : ${req.headers}`)
+                self.logger.silly(`Request header : ${JSON.stringify(req.headers)}`)
                 self._handler(stream)
             }.bind(self))
         }
