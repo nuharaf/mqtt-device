2caedf1a54cb9235fb8684a0a494c9312dde0246
Arina Nuha <nuhamind2@gmail.com>
2019-02-12 04:04:06 +0700

    add permission checking and clientid/topic format validation

--- device-broker.js
+++ device-broker.js
@@ -23,8 +23,43 @@ const config = function () {
     }
 }.call()
 
-var winston = require('winston')
+//verify config
+const Joi = require('joi');
+const schema = Joi.object().keys({
+    mqtt: Joi.object().keys({
+        protocol: Joi.string().required().valid("tcp", "tls", "ws", "wss"),
+        host: Joi.string().required(),
+        port: Joi.number().port().required(),
+        sameId: Joi.string().required().valid("prevent", "disconnect")
+    }).required(),
+    nats: Joi.object().keys({
+        host: Joi.string().required(),
+        port: Joi.number().port().required(),
+        rootTopic: Joi.string().alphanum().required(),
+    }).required(),
+    acl: Joi.object().keys({
+        connect: Joi.string(),
+        publish: Joi.string()
+    }).required(),
+    logging: Joi.object().keys({
+        level: Joi.string().required().valid("error", "warn", "info", "verbose", "debug", "silly"),
+        output: Joi.string().required()
+    }).required(),
+    api: Joi.object().keys({
+        port: Joi.number().port().required()
+    })
+})
 
+Joi.validate(config, schema, function (err, value) {
+    if (err) {
+        console.log("Invalid config")
+        console.log(err)
+        process.exit()
+    }
+})
+
+//Setup logging
+var winston = require('winston')
 const logger = winston.createLogger({
     level: config.logging.level,
     format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
@@ -86,52 +121,112 @@ var mqtt = new umqtt({
     logger: logger
 })
 
-mqtt.connectAuthenticate = function(regex,delimiter,client,done){
-    for (let segment of client.clientId.split(delimiter)){
-        if(!segment.match(regex)){
+//set callback when client attempt to connect
+mqtt.connectAuthenticate = function (regex, delimiter, client, done) {
+    for (let segment of client.clientId.split(delimiter)) {
+        if (!segment.match(regex)) {
             done(false)
             logger.error(`Invalid clientId format : ${client.clientId}`)
             return
         }
     }
-    done(true)
-}.bind(this,new RegExp('^[a-zA-Z0-9-_]+$'),".")
 
-mqtt.clientPublish = function (regex,delimiter,packet, client) {
+    //if acl connect topic specified, call it
+    if (config.acl.connect != undefined) {
+        nc.requestOne(config.acl.connect, JSON.stringify(client), {}, 1000, function (response) {
+            if (response instanceof nats.NatsError && response.code === nats.REQ_TIMEOUT) {
+                done(false)
+                return;
+            }
+            try {
+                const res = JSON.parse(response)
+                if (res.status == 'OK') {
+                    done(true)
+                }
+                else {
+                    done(false)
+                }
+            } catch (error) {
+                done(false)
+            }
+            return;
+        })
+    }
+    else {
+        done(true)
+        return
+    }
+
+}.bind(this, new RegExp('^[a-zA-Z0-9-_]+$'), ".")
+
+//set callback when client publish message
+var LRU = require("lru-cache")
+aclPublishCache = new LRU(500)
+mqtt.clientPublish = function (regex, delimiter, packet, client) {
     const topic = packet.topic
-    for (let segment of topic.split(delimiter)){
-        if(!segment.match(regex)){
+    for (let segment of topic.split(delimiter)) {
+        if (!segment.match(regex)) {
             logger.error(`Invalid topic format : ${topic}`)
             return
         }
     }
-    let payload
-    try {
-        payload = JSON.parse(packet.payload.toString());
-    } catch (error) {
-        logger.error("Invalid JSON syntax")
+
+    function forward() {
+        let payload
+        try {
+            payload = JSON.parse(packet.payload.toString());
+        } catch (error) {
+            logger.error("Invalid JSON syntax")
+        }
+        let data = {
+            topic: topic, payload: payload,
+            clientId: client.clientId, arrivalTimestamp: (new Date()).getTime()
+        }
+        if (packet.qos != 0) {
+            data.qos = packet.qos
+            data.messageId = packet.messageId
+        }
+        if (packet.dup) { data.dup = true }
+        nc.publish(`${config.nats.rootTopic}.clientPublish.${client.clientId}.${topic}`,
+            JSON.stringify(data)
+        )
     }
 
-    let data = {
-        topic: topic, payload: payload,
-        clientId: client.clientId, arrivalTimestamp: (new Date()).getTime()
+    const aclKey = `${client.clientId}+${packet.topic}`
+    if (config.acl.publish != undefined) {
+        //check cache
+        if (aclPublishCache.get(aclKey) == undefined) {
+            //invoke permission request check
+            nc.requestOne(config.acl.publish, JSON.stringify({ clientId: client.clientId, topic: packet.topic }),
+                {}, 1000, function (response) {
+                    if (response instanceof nats.NatsError && response.code === nats.REQ_TIMEOUT) {
+                        return;
+                    }
+                    const res = JSON.parse(response)
+                    if (res.status == 'OK') {
+                        aclPublishCache.set(aclKey, true)
+                        forward()
+                    }
+                    else {
+                        aclPublishCache.set(aclKey, false)
+                    }
+                    return;
+                })
+        }
+        else {
+            if (aclPublishCache.get(aclKey)) {
+                forward()
+            }
+        }
     }
-    if (packet.qos != 0) {
-        data.qos = packet.qos
-        data.messageId = packet.messageId
-    }
-    if (packet.dup) { data.dup = true }
-    nc.publish(`${config.nats.rootTopic}.clientPublish.${client.clientId}.${topic}`,
-        JSON.stringify(data)
-    )
-}.bind(this,new RegExp('^[a-zA-Z0-9-_]+$'),".")
+
+}.bind(this, new RegExp('^[a-zA-Z0-9-_]+$'), ".")
 
 mqtt.clientDisconnect = function (client) {
     nc.publish(`${config.nats.rootTopic}.clientDisconnect.${client.clientId}`)
 }
 
-
-
+//run the mqttserver
 mqtt.setup(function () {
     mqtt.run()
     nc.subscribe(`${config.nats.rootTopic}.serverPublish.*.*`, function (msg, reply, subj) {
